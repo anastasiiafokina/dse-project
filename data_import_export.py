f939fec56da04807df5b03020d0ac16191860156
anastasiiafokina <fokina.an@gmail.com>
2024-11-10 12:45:42 +0100

    Delete data_import_export.py

--- data_import_export.py
+++
@@ -1,7 +0,0 @@
-# modules/data_import_export.py
-# Manage data import and export (e.g., outputting travel summaries).
-import json
-
-def save_summary(data, filename):
-    with open(filename, 'w') as file:
-        json.dump(data, file)
